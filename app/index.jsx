998655bffd7e83c540a51fbdd2d7a367602da643
Elliot Stokes <elliot.stokes@gmail.com>
2016-04-28 09:39:09 +0100

    initial redux integration

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import {routes} from './config/routes';
+
 
 ReactDOM.render(routes, document.getElementById('app'))
